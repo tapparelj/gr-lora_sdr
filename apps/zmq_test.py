cb1569f25387bc41aeace64df9a35b55df1ae419
Martyn <martijnvdijke600@gmail.com>
2021-03-24 16:36:32 +0100

    update on plot spectogram script

--- apps/zmq_test.py
+++ apps/zmq_test.py
@@ -8,9 +8,24 @@
 # Title: Test
 # GNU Radio version: 3.8.2.0
 
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
 from gnuradio import blocks
 from gnuradio import gr
-from gnuradio.filter import firdes
 import sys
 import signal
 from argparse import ArgumentParser
@@ -18,11 +33,40 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import lora_sdr
 
+from gnuradio import qtgui
 
-class zmq_test(gr.top_block):
+class zmq_test(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Test")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Test")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "zmq_test")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
 
         ##################################################
         # Variables
@@ -30,7 +74,7 @@ class zmq_test(gr.top_block):
         self.sf = sf = 7
         self.samp_rate = samp_rate = 250000
         self.pay_len = pay_len = 64
-        self.n_frame = n_frame = 1
+        self.n_frame = n_frame = 100
         self.multi_control = multi_control = True
         self.mult_const = mult_const = 1
         self.mean = mean = 200
@@ -43,20 +87,39 @@ class zmq_test(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
         self.lora_sdr_hier_tx_0 = lora_sdr.hier_tx(pay_len, n_frame, "PKdhtXMmr18n2L9K88eMlGn7CcctT9RwKSB1FebW397VI5uG1yhc3uavuaOb9vyJ", cr, sf, impl_head,has_crc, samp_rate, bw, mean, True)
-        self.lora_sdr_frame_detector_0 = lora_sdr.frame_detector(samp_rate,bw,sf)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0, 0), (self.lora_sdr_frame_detector_0, 0))
-        self.connect((self.lora_sdr_frame_detector_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_sink_x_0, 0))
         self.connect((self.lora_sdr_hier_tx_0, 0), (self.blocks_throttle_0, 0))
 
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "zmq_test")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
 
     def get_sf(self):
         return self.sf
@@ -70,6 +133,7 @@ class zmq_test(gr.top_block):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
 
     def get_pay_len(self):
         return self.pay_len
@@ -136,21 +200,34 @@ class zmq_test(gr.top_block):
 
 
 def main(top_block_cls=zmq_test, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
     tb = top_block_cls()
 
-    def sig_handler(sig=None, frame=None):
-        tb.stop()
-        tb.wait()
+    tb.start()
 
-        sys.exit(0)
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    tb.start()
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
 
-    tb.wait()
+    def quitting():
+        tb.stop()
+        tb.wait()
 
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
 
 if __name__ == '__main__':
     main()
