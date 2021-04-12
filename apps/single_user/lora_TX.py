bf9ca721134c3bb6fb02eadb8cc16ebe7c57ed21
joachimtapp <joachim.tapparel@epfl.ch>
2021-04-12 14:18:28 +0200

    cleaned grc files

--- apps/single_user/lora_TX.py
+++ apps/single_user/lora_TX.py
@@ -17,8 +17,9 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
 import lora_sdr
-import soapy
 
 
 class lora_TX(gr.top_block):
@@ -42,83 +43,21 @@ class lora_TX(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
-        self.soapy_sink_0 = None
-        if "lime" == 'custom':
-            dev = 'driver=lime_sdr'
-            devname = dev.split("=", 1)[1]
-        else:
-            dev = 'driver=' + "lime"
-            devname = "lime"
-
-        self.soapy_sink_0 = soapy.sink(1, dev, '', samp_rate, "fc32", '')
-
-        self.soapy_sink_0.set_gain_mode(0,False)
-        self.soapy_sink_0.set_gain_mode(1,False)
-
-        self.soapy_sink_0.set_frequency(0, center_freq)
-        self.soapy_sink_0.set_frequency(1, 100.0e6)
-
-        # Made antenna sanity check more generic
-        antList = self.soapy_sink_0.listAntennas(0)
-
-        if len(antList) > 1:
-            # If we have more than 1 possible antenna
-            if len('Auto') == 0 or 'Auto' not in antList:
-                print("ERROR: Please define ant0 to an allowed antenna name.")
-                strAntList = str(antList).lstrip('(').rstrip(')').rstrip(',')
-                print("Allowed antennas: " + strAntList)
-                exit(0)
-
-            self.soapy_sink_0.set_antenna(0,'Auto')
-
-        if 1 > 1:
-            antList = self.soapy_sink_0.listAntennas(1)
-            # If we have more than 1 possible antenna
-            if len(antList) > 1:
-                if len('TX') == 0 or 'TX' not in antList:
-                    print("ERROR: Please define ant1 to an allowed antenna name.")
-                    strAntList = str(antList).lstrip('(').rstrip(')').rstrip(',')
-                    print("Allowed antennas: " + strAntList)
-                    exit(0)
-
-                self.soapy_sink_0.set_antenna(1,'TX')
-
-        # Setup IQ Balance
-        if devname != 'uhd' and devname != 'lime':
-            if (self.soapy_sink_0.IQ_balance_support(0)):
-                self.soapy_sink_0.set_iq_balance(0,0)
-
-            if (self.soapy_sink_0.IQ_balance_support(1)):
-                self.soapy_sink_0.set_iq_balance(1,0)
-
-        # Setup Frequency correction
-        if (self.soapy_sink_0.freq_correction_support(0)):
-            self.soapy_sink_0.set_frequency_correction(0,0)
-
-        if (self.soapy_sink_0.freq_correction_support(1)):
-            self.soapy_sink_0.set_frequency_correction(1,0)
-
-        if devname == 'sidekiq' or "True" == 'False':
-            self.soapy_sink_0.set_gain(0,10)
-            self.soapy_sink_0.set_gain(1,0)
-        else:
-            if devname == 'bladerf':
-                 self.soapy_sink_0.set_gain(0,"txvga1", -35)
-                 self.soapy_sink_0.set_gain(0,"txvga2", 0)
-            elif devname == 'uhd':
-                self.soapy_sink_0.set_gain(0,"PGA", 24)
-                self.soapy_sink_0.set_gain(1,"PGA", 0)
-            else:
-                 self.soapy_sink_0.set_gain(0,"PGA", 24)
-                 self.soapy_sink_0.set_gain(1,"PGA", 0)
-                 self.soapy_sink_0.set_gain(0,"PAD", 20)
-                 self.soapy_sink_0.set_gain(1,"PAD", 0)
-                 self.soapy_sink_0.set_gain(0,"IAMP", 10)
-                 self.soapy_sink_0.set_gain(1,"IAMP", 0)
-                 self.soapy_sink_0.set_gain(0,"txvga1", -35)
-                 self.soapy_sink_0.set_gain(0,"txvga2", 0)
-                 # Only hackrf uses VGA name, so just ch0
-                 self.soapy_sink_0.set_gain(0,"VGA", 10)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("addr=192.168.10.4", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            'frame_len',
+        )
+        self.uhd_usrp_sink_0.set_center_freq(center_freq, 0)
+        self.uhd_usrp_sink_0.set_gain(TX_gain, 0)
+        self.uhd_usrp_sink_0.set_antenna('TX/RX', 0)
+        self.uhd_usrp_sink_0.set_bandwidth(bw, 0)
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        # No synchronization enforced.
         self.lora_sdr_whitening_0 = lora_sdr.whitening()
         self.lora_sdr_modulate_0 = lora_sdr.modulate(sf, bw, bw, [8,16])
         self.lora_sdr_modulate_0.set_min_output_buffer(10000000)
@@ -127,8 +66,6 @@ class lora_TX(gr.top_block):
         self.lora_sdr_hamming_enc_0 = lora_sdr.hamming_enc(cr, sf)
         self.lora_sdr_gray_decode_0 = lora_sdr.gray_decode(sf)
         self.lora_sdr_add_crc_0 = lora_sdr.add_crc(has_crc)
-        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_gr_complex*1, '', "")
-        self.blocks_tag_debug_0.set_display(True)
         self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("hello world"), frame_period)
 
 
@@ -142,8 +79,7 @@ class lora_TX(gr.top_block):
         self.connect((self.lora_sdr_hamming_enc_0, 0), (self.lora_sdr_interleaver_0, 0))
         self.connect((self.lora_sdr_header_0, 0), (self.lora_sdr_add_crc_0, 0))
         self.connect((self.lora_sdr_interleaver_0, 0), (self.lora_sdr_gray_decode_0, 0))
-        self.connect((self.lora_sdr_modulate_0, 0), (self.blocks_tag_debug_0, 0))
-        self.connect((self.lora_sdr_modulate_0, 0), (self.soapy_sink_0, 0))
+        self.connect((self.lora_sdr_modulate_0, 0), (self.uhd_usrp_sink_0, 0))
         self.connect((self.lora_sdr_whitening_0, 0), (self.lora_sdr_header_0, 0))
 
 
@@ -153,6 +89,7 @@ class lora_TX(gr.top_block):
     def set_bw(self, bw):
         self.bw = bw
         self.set_samp_rate(self.bw)
+        self.uhd_usrp_sink_0.set_bandwidth(self.bw, 0)
 
     def get_sf(self):
         return self.sf
@@ -165,6 +102,7 @@ class lora_TX(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
     def get_impl_head(self):
         return self.impl_head
@@ -196,13 +134,14 @@ class lora_TX(gr.top_block):
 
     def set_center_freq(self, center_freq):
         self.center_freq = center_freq
-        self.soapy_sink_0.set_frequency(0, self.center_freq)
+        self.uhd_usrp_sink_0.set_center_freq(self.center_freq, 0)
 
     def get_TX_gain(self):
         return self.TX_gain
 
     def set_TX_gain(self, TX_gain):
         self.TX_gain = TX_gain
+        self.uhd_usrp_sink_0.set_gain(self.TX_gain, 0)
 
 
 
