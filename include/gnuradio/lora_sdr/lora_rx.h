d9b733d9b500a0769333aa0e5cf129f714f668d3
alon <alon@cariboulabs.co>
2024-02-26 13:13:00 +0200

    upp

--- include/gnuradio/lora_sdr/lora_rx.h
+++ include/gnuradio/lora_sdr/lora_rx.h
@@ -1,86 +1,108 @@
 #ifndef LORA_RX_HPP
 #define LORA_RX_HPP
-/********************
-GNU Radio C++ Flow Graph Header File
 
-Title: lora_rx
-GNU Radio version: v3.11.0.0git-645-g506e68df
-********************/
-
-/********************
-** Create includes
-********************/
+#include <gnuradio/lora_sdr/api.h>
 #include <gnuradio/top_block.h>
-#include <gnuradio/analog/sig_source.h>
-#include <gnuradio/blocks/multiply.h>
-
-
-#include <boost/program_options.hpp>
+#include <gnuradio/lora_sdr/crc_verif.h>
+#include <gnuradio/lora_sdr/deinterleaver.h>
+#include <gnuradio/lora_sdr/dewhitening.h>
+#include <gnuradio/lora_sdr/fft_demod.h>
+#include <gnuradio/lora_sdr/frame_sync.h>
+#include <gnuradio/lora_sdr/gray_mapping.h>
+#include <gnuradio/lora_sdr/hamming_dec.h>
+#include <gnuradio/lora_sdr/header_decoder.h>
 
 namespace gr {
   namespace lora_sdr {
-  
-  class LORA_SDR_API lora_sdr_rx : public gr::heir_block2 {
-  
 
-typedef std::shared_ptr<mult> mult_sptr;
-mult_sptr make_mult();
+    class LORA_SDR_API lora_sdr_rx : public gr::hier_block2 {
+    private:
+        lora_sdr::header_decoder::sptr lora_sdr_header_decoder_0;
+        lora_sdr::hamming_dec::sptr lora_sdr_hamming_dec_0;
+        lora_sdr::gray_mapping::sptr lora_sdr_gray_mapping_0;
+        lora_sdr::frame_sync::sptr lora_sdr_frame_sync_0;
+        lora_sdr::fft_demod::sptr lora_sdr_fft_demod_0;
+        lora_sdr::dewhitening::sptr lora_sdr_dewhitening_0;
+        lora_sdr::deinterleaver::sptr lora_sdr_deinterleaver_0;
+        lora_sdr::crc_verif::sptr lora_sdr_crc_verif_0;
+    public:
+        lora_sdr_rx(int bw,
+                    uint8_t cr,
+                    bool has_crc,
+                    uint8_t impl_head,
+                    uint8_t pay_len,
+                    uint32_t samp_rate,
+                    uint8_t sf,
+                    bool soft_decoding,
+                    uint8_t ldro,
+                    std::vector<bool> print_rx);
+        ~lora_sdr_rx() {};
+    };
 
-class mult : public hier_block2 {
+    //overload the constructor
+    lora_sdr_rx::lora_sdr_rx(int bw,
+                            uint8_t cr,
+                            bool has_crc,
+                            uint8_t impl_head,
+                            uint8_t pay_len,
+                            uint32_t samp_rate,
+                            uint8_t sf,
+                            bool soft_decoding,
+                            uint8_t ldro_mode,
+                            std::vector<bool> print_rx) : gr::hier_block2("lora_sdr_rx",
+                                                            gr::io_signature::make(1, 1, sizeof(gr_complex)*1),
+                                                            gr::io_signature::make(1, 1, sizeof(char)*1)) {
 
-private:
-    blocks::multiply_cc::sptr blocks_multiply_xx_0;
-    analog::sig_source_c::sptr analog_const_source_x_0;
+        uint32_t center_freq = 868100000;
+        uint16_t preamble_len = 8;
+        std::vector<uint16_t> sync_word = {18};
+        bool print_header = print_rx[0];
+        bool print_payload = print_rx[1];
 
-// Parameters:
-    gr_complex mult_amount = {0, 0};
+        this->lora_sdr_header_decoder_0 = lora_sdr::header_decoder::make(impl_head, cr, pay_len, has_crc, ldro_mode, print_header);
+        this->lora_sdr_hamming_dec_0 = lora_sdr::hamming_dec::make(soft_decoding);
+        this->lora_sdr_gray_mapping_0 = lora_sdr::gray_mapping::make(soft_decoding);
+        this->lora_sdr_frame_sync_0 = lora_sdr::frame_sync::make(center_freq, bw, sf, impl_head, sync_word, int(samp_rate/bw), preamble_len);
+        this->lora_sdr_fft_demod_0 = lora_sdr::fft_demod::make(soft_decoding, true);
+        this->lora_sdr_dewhitening_0 = lora_sdr::dewhitening::make();
+        this->lora_sdr_deinterleaver_0 = lora_sdr::deinterleaver::make(soft_decoding);
+        this->lora_sdr_crc_verif_0 = lora_sdr::crc_verif::make(print_payload, false);
 
-
-public:
-    mult(gr_complex mult_amount);
-    ~mult();
-
-    gr_complex get_mult_amount () const;
-    void set_mult_amount(gr_complex mult_amount);
-
-};
-
-mult::mult (gr_complex mult_amount) : hier_block2("mult_block",
-                gr::io_signature::make(1, 1, sizeof(gr_complex)*1),
-                gr::io_signature::make(1, 1, sizeof(gr_complex)*1)
-        ) {
-
-
-// Blocks:
-    {
-        this->blocks_multiply_xx_0 = blocks::multiply_cc::make(1);
+        hier_block2::connect(self(),0, this->lora_sdr_frame_sync_0, 0);
+        hier_block2::connect(this->lora_sdr_frame_sync_0, 0, this->lora_sdr_fft_demod_0, 0);
+        hier_block2::connect(this->lora_sdr_fft_demod_0, 0, this->lora_sdr_gray_mapping_0, 0);
+        hier_block2::connect(this->lora_sdr_gray_mapping_0, 0, this->lora_sdr_deinterleaver_0, 0);
+        hier_block2::connect(this->lora_sdr_deinterleaver_0, 0, this->lora_sdr_hamming_dec_0, 0);
+        hier_block2::connect(this->lora_sdr_hamming_dec_0, 0, this->lora_sdr_header_decoder_0, 0);
+        hier_block2::connect(this->lora_sdr_header_decoder_0, 0, this->lora_sdr_dewhitening_0, 0);
+        hier_block2::connect(this->lora_sdr_dewhitening_0, 0, this->lora_sdr_crc_verif_0, 0);
+        hier_block2::connect(this->lora_sdr_crc_verif_0, 0, self(), 0);
     }
-    {
-        this->analog_const_source_x_0 = analog::sig_source_c::make(0, analog::GR_CONST_WAVE, 0, 0, mult_amount);
+
+    typedef std::shared_ptr<lora_sdr_rx> sptr;
+    sptr make_lora_sdr_rx(int bw,
+                        uint8_t cr,
+                        bool has_crc,
+                        uint8_t impl_head,
+                        uint8_t pay_len,
+                        uint32_t samp_rate,
+                        uint8_t sf,
+                        bool soft_decoding,
+                        uint8_t ldro_mode,
+                        std::vector<bool> print_rx) {
+                                
+        return gnuradio::get_initial_sptr(new lora_sdr_rx(bw,
+                                                        cr,
+                                                        has_crc,
+                                                        impl_head,
+                                                        pay_len,
+                                                        samp_rate,
+                                                        sf,
+                                                        soft_decoding,
+                                                        ldro_mode,
+                                                        print_rx));
     }
-
-// Connections:
-    hier_block2::connect(this->analog_const_source_x_0, 0, this->blocks_multiply_xx_0, 1);
-    hier_block2::connect(this->blocks_multiply_xx_0, 0, self(), 0);
-    hier_block2::connect(self(), 0, this->blocks_multiply_xx_0, 0);
+  };
 }
 
-mult::~mult () {}
-
-// Callbacks:
-gr_complex mult::get_mult_amount () const {
-    return this->mult_amount;
-}
-
-void mult::set_mult_amount (gr_complex mult_amount) {
-    this->mult_amount = mult_amount;
-    this->analog_const_source_x_0->set_offset(this->mult_amount);
-}
-
-mult_sptr
-make_mult()
-{
-    return gnuradio::get_initial_sptr(new mult(gr_complex(2, 0)));
-}
 #endif
-
